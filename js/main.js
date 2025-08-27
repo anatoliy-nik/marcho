32a9a6242a7b96b9afbce960761904b50b85816d
anatoliy-nik <dessid@yandex.ru>
2025-08-27 13:16:37 +0300

    сделал секцию популярных товаров

--- js/main.js
+++ js/main.js
@@ -10,4 +10,19 @@ $(function () {
         // Your custom options
     });
 
+    $(".product-item__stars-box").starRating({
+        initialRating: 4,
+        emptyColor: '#ccccce',
+        hoverColor: '#ffc35b',
+        activeColor: '#ffc35b',
+        useGradient: false,
+        strokeWidth: 0,
+        starSize: 20,
+        minRating: 1,
+        readOnly: true,
+        callback: function (currentRating, $el) {
+            // make a server call here
+        }
+    });
+
 });
